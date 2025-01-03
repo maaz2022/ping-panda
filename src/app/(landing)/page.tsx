6a81d1ab11bb864e8ad6106aa39ba0e1ed491698
maaz2022 <maaznazeer098@gmail.com>
2025-01-03 16:47:26 +0500

    landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,6 +2,7 @@ import { Heading } from "@/components/Heading"
 import { MaxWidthWrapper } from "@/components/Max-Width-Wrapper"
 import { Check } from "lucide-react"
 import { ShinyButton } from "@/components/Shiny-Button"
+import { MockDiscordUI } from "@/components/Mock_discord_ui"
 const Page = () => {
   return (
     <>
@@ -38,7 +39,18 @@ const Page = () => {
         </div>
       </MaxWidthWrapper>
     </section>
-    <section></section>
+    <section className="bg-brand-25 relative pb-4">
+               <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+               <div className="relative mx-auto">
+                  <MaxWidthWrapper className="relative">
+                    <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                      <MockDiscordUI>
+                        
+                      </MockDiscordUI>
+                    </div>
+                  </MaxWidthWrapper>
+               </div>
+    </section>
     <section></section>
     <section></section>
     <section></section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
-import Link from "next/link"
-import { MaxWidthWrapper } from "./Max-Width-Wrapper"
-import { SignInButton, SignOutButton } from "@clerk/nextjs"
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { MaxWidthWrapper } from "./Max-Width-Wrapper";
+import { ArrowRightIcon } from "lucide-react";
 
 export const NavBar = () => {
     const user = false;
     return (
         <nav className=" sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
-            <MaxWidthWrapper>
+            <MaxWidthWrapper className="flex h-16 items-center justify-between">
                 <div className="flex h-16 items-center justify-between">
                     <Link href='/' className="flex z-40 font-semibold ">
                         Ping<span className="text-brand-700">Panda</span>
@@ -15,14 +16,47 @@ export const NavBar = () => {
                 </div>
                 <div className="h-full flex items-center space-x-4">
                     {user ? (
-                        <SignOutButton>
-                            <Button>
+                        <>
+                            <SignOutButton>
+                            <Button size={"sm"} variant={"ghost"}>
                                 Sign Out
                         </Button>
                     </SignOutButton>
+                    <Link href="/dashboard">
+                        <Button  className={buttonVariants({
+                            size: "sm",
+                            className: "flex items-center gap-1"
+                        })}>
+                            Dashboard <ArrowRightIcon className="ml-1.5 size-4"/>
+                        </Button>
+                    </Link>
+                        </>
                     ) : (
-                        null
-                    )}
+                        <>
+                        <Link href="/pricing" className={buttonVariants({
+                            size: "sm",
+                          variant: "ghost"
+                        })}>
+                            Get PingPanda 
+                       
+                    </Link>
+                    <Link href="/sign-in" className={buttonVariants({
+                        size: "sm",
+                        variant: "ghost"
+                    })}>
+                            Sign In 
+                    </Link>
+                    <div  className="h-8 w-px bg-gray-200"/>
+                    <Link href="/sign-up">
+                        <Button  className={buttonVariants({
+                            size: "sm",
+                            className: "flex items-center gap-1"
+                        })}>
+                            Sign Up <ArrowRightIcon className="ml-1.5 size-4"/>
+                        </Button>
+                    </Link>
+                    </>
+                )}
                 </div>
             </MaxWidthWrapper>
         </nav>
